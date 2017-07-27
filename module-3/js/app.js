962f0db14aaa3ccbf41c2e425852fecae1f175c5
kambainer <kam-unfcl@yandex.ru>
2017-07-27 14:27:29 +0400

    controller added, directive in progress

--- module-3/js/app.js
+++ module-3/js/app.js
@@ -5,15 +5,49 @@
     'use strict';
     angular.module('NarrowItDownApp',[])
         .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService);
+        .service('MenuSearchService', MenuSearchService)
 
-    NarrowItDownController.$inject = ['MenuSearchService'];
+        .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+        .directive('foundItems', FoundItemsDirective);
+
+    function FoundItemsDirective()
+    {
+      //routine
+      var ddo = {
+        scope: {items: '<'}
+        //controller: ShoppingListDirectiveController,
+        //controllerAs: 'list',
+        //bindToController: true
+      };
+      return ddo;
+    }
+
+
+    NarrowItDownController.$inject = ['MenuSearchService', 'ApiBasePath'];
     function NarrowItDownController(MenuSearchService){
-        //TODO inject
+      //TODO inject
+      var ctrl = this;
+      ctrl.found = MenuSearchService.getMenuCategories();
+
+      ctrl.LogSomething = function () {
+      	console.log('Controllers found: '+ ctrl.found);
+        console.log(ctrl.found);
+      }
     }
     
-    function MenuSearchService() {
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
+    function MenuSearchService($http, ApiBasePath) {
         var service = this;
+
+        service.getMenuCategories = function () {
+			    var response = $http({
+			      method: "GET",
+			      url: (ApiBasePath + "/categories.json")
+			    });
+
+			    return response;
+			  };
 
         service.getMatchedMenuItems = function (searchTerm){
             return "blah";
