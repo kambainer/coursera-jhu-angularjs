6f9139c922567404ec399485503bed2021946657
KAM <kam-unfcl@yandex.ru>
2017-07-28 23:48:37 +0400

    temp

--- module-3/js/app.js
+++ module-3/js/app.js
@@ -5,6 +5,7 @@
     'use strict';
     angular.module('NarrowItDownApp',[])
         .controller('NarrowItDownController', NarrowItDownController)
+        .controller('FoundItemsDirectiveController', FoundItemsDirectiveController)
         .service('MenuSearchService', MenuSearchService)
         .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
         .directive('foundItems', FoundItemsDirective);
@@ -16,10 +17,11 @@
         templateUrl: 'found-list.html',
         scope: {
           title: '@title',
-          found: '<'  //one-way-binding
+          found: '<',
+          onRemove: '&'
         },
-        controller: FoundItemsDirectiveController,
-        controllerAs: 'list',
+        controller: 'FoundItemsDirectiveController as list',
+        //controllerAs: 'list',
         bindToController: true
       };
       return ddo;
@@ -27,30 +29,25 @@
 
     function FoundItemsDirectiveController() {
       var list = this;
-      list.title = "TITLE IN THE DIRECTIVEs CONTROLLER";
 
-      list.cookiesInList = function () {
-          for (var i = 0; i < list.found.length; i++) {
-             var name = list.found[i].name;
-             if (name.toLowerCase().indexOf("cookies") !== -1) {
-                 return true;
-             }
-         }
-         return false;
+      //TODO lets use that for viewing nothing found message
+      list.isFoundEmpty = function () {
+          return (list.found.length === 0)
       };
     }
-
 
     NarrowItDownController.$inject = ['MenuSearchService', 'ApiBasePath'];
     function NarrowItDownController(MenuSearchService){
       //TODO inject
       var ctrl = this;
-      //ctrl.found = MenuSearchService.getMenuCategories();
-      ctrl.found = [{name: 'Cookie', count: 5},
-                    {name: 'Milk', count: 3}];
+      ctrl.found = MenuSearchService.getMatchedMenuItems("beef");
       ctrl.title="Title property, found count: " + ctrl.found.length + " items.";
-      ctrl.LogSomething = function () {
-        console.log(ctrl.found);
+      console.log(ctrl.found);
+
+      ctrl.removeItem = function (index) {
+            console.log("Hi, "+index+"!");
+            console.log('this is:',this);
+            MenuSearchService.removeItem(index);
       }
     }
     
@@ -58,18 +55,30 @@
 
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
-
-        service.getMenuCategories = function () {
-			    var response = $http({
-			      method: "GET",
-			      url: (ApiBasePath + "/categories.json")
-			    });
-
-			    return response;
-			  };
+        // List of shopping items
+        var items = [];
 
         service.getMatchedMenuItems = function (searchTerm){
-            return "blah";
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json"),
+            }).then(function (response) {
+                console.log("response:",
+                     response.data['menu_items'].filter(
+                         function MatchToSearchTerm(value){
+                             return (value['name'].toLowerCase().indexOf(searchTerm) !== -1)
+                         })
+                 );
+            });
+
+
+
+            items = new Array({name: 'Cookie', count: 5}, {name: 'Milk', count: 3});
+            return items;
+        };
+
+        service.removeItem = function (itemIndex) {
+            items.splice(itemIndex, 1);
         };
 
     }
