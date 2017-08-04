676d7a75621d4182b237b7665702fc4275620167
Кондратьев Александр Михайлович <kondratyevam@intranet.vdcom>
2017-08-04 18:03:58 +0400

    states routing routine

--- module-4/lib/categories.component.js
+++ module-4/lib/categories.component.js
@@ -1,8 +1,41 @@
 /**
  * Created by kondratyevam on 03.08.2017.
  */
-angular.module('data').component('categories',CategoriesComponent);
+angular.module('data')
+    .controller('CategoriesController',CategoriesController)
+    .component('categories',{
+        templateUrl: 'categories.html',
+        controller: CategoriesCompController,
+        bindings: {
+            items: '<',
+            myTitle: '@title',
+            getCategories: '&'
+        }
+    });
 
-function CategoriesComponent() {
+function CategoriesCompController() {
     //TODO
+    var $ctrl = this;
+
+    $ctrl.$onInit = function () {
+        console.log('$onInit');
+        $ctrl.getCategories();
+        console.log($ctrl);
+    };
+}
+CategoriesController.$inject=['MenuDataService'];
+function CategoriesController(MenuDataService) {
+    var cats = this;
+    cats.title = "This is cats title";
+    cats.items=[];
+
+    cats.GetCats = function () {
+        console.log('onGetCats');
+        var promise = MenuDataService.getAllCategories();
+        promise.then(function (result) {
+            cats.items = result;
+            console.log(cats.items);
+        });
+    };
+
 }
--- module-4/lib/menudata.service.js
+++ module-4/lib/menudata.service.js
@@ -2,27 +2,48 @@
  * Created by kondratyevam on 03.08.2017.
  */
 angular.module('data')
-    .service('MenuDataService', MenuDataService)
-    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+    .service('MenuDataService', MenuDataService);
+
 
 MenuDataService.$inject = ['$http', 'ApiBasePath'];
 function MenuDataService ($http, ApiBasePath) {
     var service = this;
     console.log('MenuDataService:'+ApiBasePath);
 
-    service.getAllCategories = function ($http) {
+    service.getFakeCategories = function() {
+        var categories = [{id: 81, short_name: 'fake_1'},{id:2,short_name:'fake_2'}];
+        return categories;
+    };
+
+    service.getAllCategories = function () {
         var response = $http({
             method: "GET",
             url: (ApiBasePath + "/categories.json"),
         }).then(
             function (response) {
-                console.log('response',response.data);
+                return response.data;
             }
-        );
+        ).catch(function (error) {
+            console.log('getAllCategories error: ', error);
+        });
+
+        return response;
     };
 
     service.getItemsForCategory = function(categoryShortName){
-        //todo
-        console.log('getItemsForCategory(categoryShortName)');
+        var response = $http({
+            method: "GET",
+            url: (ApiBasePath + "/menu_items.json"),
+            params: {category: categoryShortName}
+        }).then(
+            function (response) {
+                return response.data;
+            }
+        ).catch(function (error) {
+            console.log('getAllCategories error: ', error);
+        });
+
+        return response;
     };
 }
